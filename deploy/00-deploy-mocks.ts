19babb510f5304053d0164a7a345a933067e1201
ghaieth96 <ghaiethbmoussa@gmail.com>
2023-01-29 13:40:27 +0100

    deploy mock VRFCoordinator

---
+++ deploy/00-deploy-mocks.ts
@@ -0,0 +1,29 @@
+import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { developmentChains } from "../helper-hardhat-config";
+
+const BASE_FEE = ethers.utils.parseEther("0.25"); // 0.25 is the premium. It costs 0.25 LINK per request.
+const GAS_PRICE_LINK = 1e9; // Link per gas. Calculated value based on the gas price of the chain.
+
+const deployMocks: DeployFunction = async function ({
+  getNamedAccounts,
+  deployments,
+  network,
+}: HardhatRuntimeEnvironment) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId: number = network.config.chainId!;
+
+  if (developmentChains.includes(network.name)) {
+    await deploy("VRFCoordinatorV2Mock", {
+      contract: "VRFCoordinatorV2Mock",
+      from: deployer,
+      log: true,
+      args: [BASE_FEE, GAS_PRICE_LINK],
+    });
+  }
+};
+
+export default deployMocks;
+deployMocks.tags = ["all", "mocks", "main"];
